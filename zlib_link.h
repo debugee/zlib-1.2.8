7c7ce206071a1ea9c15d21a0e4a6c253256dc1f2
debugee <519306795@qq.com>
2023-07-25 20:01:54 +0800

    link.h

--- zlib_link.h
+++ zlib_link.h
@@ -1,7 +1,32 @@
 #pragma once
 
+
 #ifdef _DEBUG
-    #pragma comment(lib, "v12_smdd_zlibstat.lib")
+	#ifdef _DLL
+		#if _MSC_VER == 1800
+			#pragma comment(lib, "v12_smdd_zlibstat.lib")
+		#elif _MSC_VER == 1900
+			#pragma comment(lib, "v14_smdd_zlibstat.lib")
+		#endif
+	#else
+		#if _MSC_VER == 1800
+			#pragma comment(lib, "v12_smtd_zlibstat.lib")
+		#elif _MSC_VER == 1900
+			#pragma comment(lib, "v14_smtd_zlibstat.lib")
+		#endif
+	#endif
 #else
-    #pragma comment(lib, "v12_smd_zlibstat.lib")
+	#ifdef _DLL
+		#if _MSC_VER == 1800
+			#pragma comment(lib, "v12_smd_zlibstat.lib")
+		#elif _MSC_VER == 1900
+			#pragma comment(lib, "v14_smd_zlibstat.lib")
+		#endif
+	#else
+		#if _MSC_VER == 1800
+			#pragma comment(lib, "v12_smt_zlibstat.lib")
+		#elif _MSC_VER == 1900
+			#pragma comment(lib, "v14_smt_zlibstat.lib")
+		#endif
+	#endif
 #endif
