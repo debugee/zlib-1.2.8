c183815a85fc200cd059b9b2ab72aafa9ead827b
debugee <519306795@qq.com>
2024-07-22 21:02:26 +0800

    MSVC_TOOLSET_VERSION

--- zlib_link.h
+++ zlib_link.h
@@ -3,29 +3,29 @@
 
 #ifdef _DEBUG
 	#ifdef _DLL
-		#if _MSC_VER == 1800
+		#if MSVC_TOOLSET_VERSION == 120
 			#pragma comment(lib, "v12_smdd_zlibstat.lib")
-		#elif _MSC_VER == 1900
+		#else
 			#pragma comment(lib, "v14_smdd_zlibstat.lib")
 		#endif
 	#else
-		#if _MSC_VER == 1800
+		#if MSVC_TOOLSET_VERSION == 120
 			#pragma comment(lib, "v12_smtd_zlibstat.lib")
-		#elif _MSC_VER == 1900
+		#else
 			#pragma comment(lib, "v14_smtd_zlibstat.lib")
 		#endif
 	#endif
 #else
 	#ifdef _DLL
-		#if _MSC_VER == 1800
+		#if MSVC_TOOLSET_VERSION == 120
 			#pragma comment(lib, "v12_smd_zlibstat.lib")
-		#elif _MSC_VER == 1900
+		#else
 			#pragma comment(lib, "v14_smd_zlibstat.lib")
 		#endif
 	#else
-		#if _MSC_VER == 1800
+		#if MSVC_TOOLSET_VERSION == 120
 			#pragma comment(lib, "v12_smt_zlibstat.lib")
-		#elif _MSC_VER == 1900
+		#else
 			#pragma comment(lib, "v14_smt_zlibstat.lib")
 		#endif
 	#endif
